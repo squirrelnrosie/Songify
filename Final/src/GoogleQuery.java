70f9a0781409c6f459e2bb30101ee181e25874e8
winniechiou <s410349@shsh.tw>
2020-01-02 12:25:10 +0800

    加入相關搜尋

--- Final/src/GoogleQuery.java
+++ Final/src/GoogleQuery.java
@@ -199,6 +199,7 @@ public class GoogleQuery
 			System.out.println("相關搜尋結果："+chineselist);
 			
 			
+			
 		}
 		
 		
--- Final/src/Main.java
+++ Final/src/Main.java
@@ -125,6 +125,7 @@ public class Main {
 		
 		
 		
+		
 		/**
 		try{
 			Subpage subpage = new Subpage();
